832e36dceff41fe1610108d8b730235f30c970b8
Indranil Halder <indranilhalder.dev@gmail.com>
2023-06-26 00:40:24 +0530

    Integrated Redux Toolkit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
-import "bootswatch/dist/zephyr/bootstrap.min.css";
 import Router from "./Router/Router";
+import store from "./store/store";
+// Bootswatch bootstrap
+import "bootswatch/dist/zephyr/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={Router} />
+    <Provider store={store}>
+      <RouterProvider router={Router} />
+    </Provider>
   </React.StrictMode>
 );
