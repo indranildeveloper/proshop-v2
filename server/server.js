fb858f84879063dd438a900b087e3e105f34b7fe
Indranil Halder <indranilhalder.dev@gmail.com>
2023-06-26 00:02:19 +0530

    Created controllers and middlewares

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ import express from "express";
 import colors from "colors";
 import { config } from "dotenv";
 import connectDB from "./config/db";
-import products from "./data/products";
+import { notFound, errorHandler } from "./middleware/errorMiddleware";
+import productRoutes from "./routes/productRoutes";
 
 config();
 
@@ -17,13 +18,9 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+app.use("/api/products", productRoutes);
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
