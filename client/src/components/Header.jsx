958e7b8abe9631ad1070a54941187161d62b8367
Indranil Halder <indranilhalder.dev@gmail.com>
2023-06-29 03:05:30 +0530

    Display Cart Items

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,15 @@
+import { useSelector } from "react-redux";
 import { LinkContainer } from "react-router-bootstrap";
+import Badge from "react-bootstrap/Badge";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import Stack from "react-bootstrap/Stack";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 
 const Header = () => {
+  const { cartItems } = useSelector((state) => state.cart);
+
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
@@ -17,7 +22,18 @@ const Header = () => {
             <Nav className="ms-auto">
               <LinkContainer to="/cart">
                 <Nav.Link>
-                  <FaShoppingCart /> Cart
+                  <Stack
+                    direction="horizontal"
+                    gap={2}
+                    className="align-items-center"
+                  >
+                    <FaShoppingCart /> Cart
+                    {cartItems.length > 0 && (
+                      <Badge bg="light" as="span">
+                        {cartItems.reduce((acc, cur) => acc + cur.qty, 0)}
+                      </Badge>
+                    )}
+                  </Stack>
                 </Nav.Link>
               </LinkContainer>
               <LinkContainer to="/login">
