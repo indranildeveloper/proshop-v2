49508379d9abc34c777f4117164c515d148a7893
Indranil Halder <indranilhalder.dev@gmail.com>
2023-07-08 19:42:15 +0530

    Upload Image Backend

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 /* eslint-disable-next-line no-unused-vars */
 import colors from "colors";
@@ -8,6 +9,7 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 
 config();
 
@@ -23,6 +25,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
@@ -30,6 +35,7 @@ app.get("/", (req, res) => {
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
 
 /**
  * ===========Paypal Payment Integration=================
